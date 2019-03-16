4212203e3b24d85c30bb9617ea58d7b222fe13ad
dardalan <svyat.9191@gmail.com>
2019-03-16 13:06:47 +0200

    initialize exercism repo

---
+++ javascript/htmlBuilder/buildNode.js
@@ -0,0 +1,9 @@
+import SingleTag from './SingleTag'
+import PairedTag from './PairedTag'
+
+const singleTagsList = new Set(['hr', 'br', 'img']);
+
+export default (name, ...args) => {
+  const C = singleTagsList.has(name) ? SingleTag : PairedTag;
+  return new C(name, ...args);
+};
